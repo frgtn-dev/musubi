2ba8ac140a7f8dafa4e5b4b392ab9999cf751fcf
Filip Tůma <flamendrin@gmail.com>
2026-05-05 20:29:46 +0200

    Docs Dockerfile

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -2,29 +2,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Musubi',
-			customCss: ['./src/styles/custom.css'],
-			sidebar: [
-				{
-					label: 'Getting Started',
-					items: [
-						{ label: 'Introduction', slug: 'guides/introduction' },
-						{ label: 'Running Locally', slug: 'guides/running-locally' },
-					],
-				},
-				{
-					label: 'App',
-					items: [
-						{ label: 'Client', slug: 'reference/client' },
-						{ label: 'Server', slug: 'reference/server' },
-						{ label: 'Database Schema', slug: 'reference/schema' },
-					],
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Musubi',
+          customCss: ['./src/styles/custom.css'],
+          sidebar: [
+              {
+                  label: 'Getting Started',
+                  items: [
+                      { label: 'Introduction', slug: 'guides/introduction' },
+                      { label: 'Running Locally', slug: 'guides/running-locally' },
+                  ],
+              },
+              {
+                  label: 'App',
+                  items: [
+                      { label: 'Client', slug: 'reference/client' },
+                      { label: 'Server', slug: 'reference/server' },
+                      { label: 'Database Schema', slug: 'reference/schema' },
+                  ],
+              },
+          ],
+      }),
 	],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
